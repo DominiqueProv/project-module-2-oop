a00da6aaa9ecb6dc76976c13f8d5aee1b7a26380
Dominiqueprov <dominiquepro@hotmail.com>
2020-02-17 09:54:03 -0500

    Game update
    my game update

--- js/data.js
+++ js/data.js
@@ -1,21 +1,42 @@
-// In this file we have some data that the other source files will use.
-// Most of this data is stored in constants.
-// Constants are just variables that never change. By convention,
-// We write constants with upper case letters.
 
-// The GAME_WIDTH and GAME_HEIGHT constants denote the size
-// of the game area in pixels and is used in engine-utilities.js.
 const GAME_WIDTH = 375;
 const GAME_HEIGHT = 500;
 
-// These constants represent the width and height of an enemy in pixels
-// as well as the maximum number of enemies on screen at any given time.
 const ENEMY_WIDTH = 75;
-const ENEMY_HEIGHT = 156;
+const ENEMY_HEIGHT = 72;
 const MAX_ENEMIES = 3;
 
-// These constants represent the player width and height.
 const PLAYER_WIDTH = 75;
 const PLAYER_HEIGHT = 54;
 
 
+
+
+class Score {
+    constructor(root, xPos, yPos) {
+        // We create a DOM element, set its CSS attributes then append it to the parent DOM element. We also
+        // set the \`domElement\` property of the instance to the newly created DOM element so we can update it later
+        this.score = 0;
+        const div = document.createElement("div");
+        div.id = "score"
+        div.style.position = 'absolute'
+        div.style.left = xPos;
+        div.style.top = yPos;
+        div.style.color = 'black';
+        div.style.font = 'bold 30px Impact';
+        div.style.zIndex = 2000;
+        div.innerText = "SCORE:" + this.score;
+        root.appendChild(div);
+        this.domElement = div;
+    }
+    update() {
+        this.score = this.score + 10;
+        if (this.score >= 2000 && this.score <= 5000) {
+            this.domElement.style.color = "gray";
+        } else if (this.score > 5000) {
+            this.domElement.style.color = "dodgerblue";
+        }
+        this.domElement.innerText = "SCORE:" + this.score;
+
+    } 
+}
